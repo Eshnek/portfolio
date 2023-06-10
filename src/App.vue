234bc233baff4a1ab940657c24c0d5ba96de8ee0
Eshnek <dylanpozarnsky@gmail.com>
2023-06-10 04:37:29 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import TopBar from "./components/TopBar.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <TopBar />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/components/TopBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+
+    a {
+        padding: 8px;
+    }
+}
+</style>
