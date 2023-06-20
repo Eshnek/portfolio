8c6621caf887170b4b356efdbe1208b7e050f5e8
Eshnek <dylanpozarnsky@gmail.com>
2023-06-20 14:54:04 -0600

    6/20/2023 14:54 +

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,9 +2,12 @@
     <nav class="top-bar">
         <RouterLink to="/">Portfolio</RouterLink>
         <RouterLink to="/about">Resume</RouterLink>
-        <a href="https://github.com/Eshnek" target="_blank">
-            <img src="logos/github/github-mark.svg" alt="Github">
-        </a>
+        <RouterLink to="/about">Github</RouterLink>
+        <div class="top-right">
+            <a href="https://github.com/Eshnek" target="_blank">
+                <img src="logos/github/github-mark.svg" alt="Github">
+            </a>
+        </div>
     </nav>
 </template>
 
@@ -16,17 +19,17 @@
     font-size: 12px;
     text-align: center;
 
-    padding: 16px;
+    padding: var(--padding-top);
 
     display: flex;
     align-items: center;
     justify-content: center;
 
-    border-bottom: 1px solid var(--color-border);
+    border-bottom: var(--border-top);
     background-color: var(--color-background-top);
 
     a {
-        padding: 8px;
+        padding: var(--padding-top-link);
         line-height: 0px;
 
         text-decoration: none;
@@ -37,6 +40,14 @@
             width: 32px;
             height: auto;
         }
+    }
+    .top-right {
+        height: inherit;
+        position: absolute;
+        right: var(--padding-top-horizontal);
+
+        display: flex;
+        align-items: center;
     }
 }
 </style>
