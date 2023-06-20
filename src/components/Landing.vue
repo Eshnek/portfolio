424855356ebeaba5fa9fb57466f390c3ea7cf3c0
Eshnek <dylanpozarnsky@gmail.com>
2023-06-20 14:34:35 -0600

    6/20/2023 14:34 +

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,33 +2,34 @@
 </script>
 
 <template>
-  <div class="greetings">
-    <h1>
-        Portfolio
-    </h1>
-  </div>
+    <div class="greetings">
+        <h1>
+            Portfolio
+        </h1>
+    </div>
 </template>
 
 <style scoped>
 h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  top: -10px;
+    font-weight: 500;
+    font-size: 2.6rem;
+    top: -10px;
 }
 
 h3 {
-  font-size: 1.2rem;
+    font-size: 1.2rem;
 }
 
 .greetings h1,
 .greetings h3 {
-  text-align: center;
+    text-align: center;
 }
 
 @media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+
+    .greetings h1,
+    .greetings h3 {
+        text-align: left;
+    }
 }
 </style>
