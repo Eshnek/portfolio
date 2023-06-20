a693dd0ed2f3a5f51920b54d6773bbfea05dd2e6
Eshnek <dylanpozarnsky@gmail.com>
2023-06-20 15:38:45 -0600

    6/20/2023 15:38 +

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -2,14 +2,20 @@
     <nav class="top-bar">
         <RouterLink to="/">Portfolio</RouterLink>
         <RouterLink to="/about">Resume</RouterLink>
-        <RouterLink to="/about">Github</RouterLink>
+        <a href="https://github.com/Eshnek" target="_blank">Github</a>
         <div class="top-right">
-            <a href="https://github.com/Eshnek" target="_blank">
-                <img src="logos/github/github-mark.svg" alt="Github">
-            </a>
+            <Corner />
         </div>
     </nav>
 </template>
+
+<script lang="ts">
+import Corner from '@/components/Corner.vue';
+
+export default {
+    components: { Corner },
+}
+</script>
 
 <style scoped lang="scss">
 .top-bar {
@@ -20,7 +26,6 @@
     text-align: center;
 
     padding: var(--padding-top);
-
     display: flex;
     align-items: center;
     justify-content: center;
@@ -35,16 +40,12 @@
         text-decoration: none;
         font-size: var(--font-size-top);
         color: var(--color-text);
-
-        img {
-            width: 32px;
-            height: auto;
-        }
     }
+
     .top-right {
         height: inherit;
         position: absolute;
-        right: var(--padding-top-horizontal);
+        right: 0; // var(--padding-top-horizontal);
 
         display: flex;
         align-items: center;
