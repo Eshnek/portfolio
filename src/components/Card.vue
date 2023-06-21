6cafca6b41fd9ce40a0be5e25a793127b025eeaf
Eshnek <dylanpozarnsky@gmail.com>
2023-06-21 11:23:20 -0600

    Start of card

---
+++ src/components/Card.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="card">
+        <div class="card-title card-text">
+            <h1>{{ title }}</h1>
+        </div>
+        <div class="card-description card-text">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    props: {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+    min-width: 256px;
+    min-height: 128px;
+    max-width: 512px;
+    max-height: 1024px;
+
+    width: min-content;
+    height: min-content;
+
+    border: 1px solid var(--color-border-card);
+    border-radius: 8px;
+
+    background-color: var(--color-background-card);
+
+    padding: var(--padding-card);
+
+    box-shadow: 3px 3px 4px 0px var(--color-shadow-card);
+
+    .card-title {
+
+    }
+    .card-text {
+        width: 100%;
+
+        text-align: center;
+        overflow-wrap: break-word;
+    }
+}
+</style>
--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,13 +1,22 @@
-<script setup lang="ts">
-</script>
-
 <template>
     <div class="home">
-        <h1>
-            Portfolio Home
-        </h1>
+        <Card title="A" description="Bdfdhfdasljk;falsdjk;l;fkdaslfadslk;jfadsl;jkafdslj;kfadslkjfdslkj;afdslkj;afdlsjk;lfakdslk;jafdslkjafds dsfjkfsadjl kfadslj;kf adsl;jkfadslj;k fla dsk;jfl kaj;dsl jk;afdsl kjfads ljk"></Card>
     </div>
 </template>
 
+<script lang="ts">
+import Card from '@/components/Card.vue';
+
+export default {
+    components: { Card },
+}
+</script>
+
 <style scoped>
+.home {
+    display: flex;
+    justify-content: center;
+
+    padding: var(--padding-page);
+}
 </style>
