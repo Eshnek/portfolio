195d9bcadad6aeb7e1311e66ce790707728519fd
Eshnek <dylanpozarnsky@gmail.com>
2023-06-26 21:25:09 -0600

    Add some cards

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Discord: Eshnek#0385 Email: LinkedIn:
+    <Card
+        title="Discord"
+        description="@Eshnek"
+    />
+
+    <Card
+        title="Email"
+        description="dylanpozarnsky@gmail.com"
+    />
+
+    <Card
+        title="LinkedIn"
+        description="https://www.linkedin.com/in/dylan-pozarnsky/"
+    />
+
+    <Card
+        title="Github"
+        description="https://github.com/CitadelStronghold/teek"
+    />
 
     <span>
-      Add some pretty cards with social media links. Use the social media color
+      Use the social media color
       as background, to help the user easily identify them. Include as much info
       as possible to build credibility
     </span>
   </div>
 </template>
+
+<script lang="ts">
+import Card from '@/components/Card.vue';
+
+export default {
+    components: { Card }
+}
+</script>
